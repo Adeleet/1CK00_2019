9a11419db28ffc8ee4909b680b00b38bd01594b7
Max Adaloudis <maxadaloudis@gmail.com>
2019-06-17 20:49:41 +0200

    Clean up Exact model 1

--- scripts/1.1.py
+++ scripts/1.1.py
@@ -1,42 +1,37 @@
-import itertools
-import math
-import random
-import sys
-
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from gurobipy import *
+from gurobipy import GRB, Model, tupledict
 
-from visualizer import plot, plot_tsp
+from visualizer import plot_tsp
 
-tsp_30_distances = np.load("../data/tsp30_distances.npy")
-tsp_30_locations = np.load("../data/tsp30_locations.npy")
-distance_map = list(zip([i for i in range(30)], [i for i in range(30)]))
-n = 30
+# Import distances (30x30) and locations x,y coordinates (30x2)
+dist = np.load("../data/tsp30_distances.npy")
+location = np.load("../data/tsp30_locations.npy")
 
+# distance_map = list(zip([i for i in range(n)], [i for i in range(n)]))
 
-# distance matrix
+# Number of cities n
+n = dist.shape[0]
+print('Number of cities in TSP model: {}'.format(n))
 
-dist = tsp_30_distances
-# number of cities n
-n = len(dist)
-print('The number of cities in my model: {}'.format(n))
-
-# build model
+# 1 INITIALIZE MODEL
 m = Model()
+
+# 2 DEFINE X VARIABLES
 xvars = tupledict()
 
-# 1. Define the variables:
-# xvars for arcs leaving depot 0
+# Each depot (30) can travel to all other (29) at 30 steps
+# 2a xvars for arcs leaving depot 0 (29 routes from depot 0)
 for j in range(1, n):
-    xvars[0, j, 0] = m.addVar(
-        obj=dist[0][j], vtype=GRB.BINARY, name='x[%d,%d,%d]' % (0, j, 0))
+    xvars[0, j, 0] = m.addVar(obj=dist[0][j],
+                              vtype=GRB.BINARY,
+                              name='x[%d,%d,%d]' % (0, j, 0))
 
-# xvars for arcs entering depot 0
+# 2b xvars for arcs entering depot 0 (29 routes to depot 0)
 for i in range(1, n):
     xvars[i, 0, n - 1] = m.addVar(obj=dist[i][0],
-                                  vtype=GRB.BINARY, name='x[%d,%d,%d]' % (i, 0, n - 1))
+                                  vtype=GRB.BINARY,
+                                  name='x[%d,%d,%d]' % (i, 0, n - 1))
 
 # xvars[x,y,z]
 #     x : source
@@ -44,33 +39,37 @@ for i in range(1, n):
 #     z : time step
 
 # all remaining xvars
+# Since we always start at 0, consider other 29 depots i,j (30 - 1 = 29)
 for i in range(1, n):
     for j in range(1, n):
+        # Depot cannot travel to 'itself'
         if i != j:
+            # Because we start/end at depot 0, the first (0) and last (n) steps can be ignored; 28 steps
             for t in range(1, n - 1):
-                xvars[i, j, t] = m.addVar(
-                    obj=dist[i][j], vtype=GRB.BINARY, name='x[%d,%d,%d]' % (i, j, t))
+                # Add xvar: from i to j at step t
+                xvars[i, j, t] = m.addVar(obj=dist[i][j],
+                                          vtype=GRB.BINARY,
+                                          name='x[%d,%d,%d]' % (i, j, t))
 
+# 3 DEFINE CONSTRAINTS
 
-# 2. Define the constraints:
-
-# leave depot
+# Must start from depot 0 on first step
 m.addConstr((xvars.sum(0, '*', 0) == 1), "leaveDepot")
 
-# Arrive at leaveDepot
-m.addConstr((xvars.sum("*", 0, 29) == 1), "arriveDepot")
+# Must arrive at depot 0 on last step
+m.addConstr((xvars.sum("*", 0, n - 1) == 1), "arriveDepot")
 
-# 3 constraints are still missing here....
 m.update()
-for i in range(30):
-    for t in range(29):
-        m.addConstr(xvars.sum("*", i, t) == xvars.sum(i,
-                                                      "*", t + 1), name="incomingToutgoingT+1")
 
+# Ingoing at i at time t = Outgoing from i at time t+1
+for i in range(n):
+    for t in range(n - 1):
+        m.addConstr(xvars.sum("*", i, t) == xvars.sum(i, "*", t + 1),
+                    name="inOut_i{}_t{}".format(i, t))
 
-for i in range(30):
+# Only visit location once
+for i in range(n):
     m.addConstr(xvars.sum("*", i, "*") == 1)
-
 
 m.update()
 # Optional: write model to file
@@ -85,7 +84,6 @@ print('Obj:', m.objVal)
 
 # Build and visualize the tour here using the x variables...
 
-
 vals = m.getAttr('X', xvars)
 vals_series = pd.Series(dict(vals))
 vals_visited = vals_series[vals_series == 1]
@@ -94,8 +92,9 @@ visits = list(dict(vals_series[vals_series == 1]).keys())
 
 visits_sorted = sorted(visits, key=lambda x: x[2])
 tour = [0] + [v[1] for v in visits_sorted]
-tour
-plot_tsp(tsp_30_locations, tour, name='exact')
-[tsp_30_locations[i] for i in tour]
-tour
-# done :)
+
+plot_tsp(location, tour, name='exact')
+
+r = m.relax()
+r.optimize()
+print("LPrelax: ", r.objVal)
